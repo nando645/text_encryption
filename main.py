827d304e10ee6110671c47bfbc054a90e8ab2d2c
nando645 <fernando154715@student.esaunggul.ac.id>
2022-11-19 21:52:22 +0700

    test

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import os
+from AEScipher import AESCipher
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def enkripsi():
+    if request.method == 'POST':
+        input_text = request.form['plaintext']
+        input_key = request.form['key']
+        cipher = AESCipher(input_key)
+        encrypted = cipher.encrypt(input_text)
+        return render_template('enkripsi.html', submit=True, ciphertext=encrypted)
+    return render_template('enkripsi.html', submit=False, variabel=1234)
+
+
+@app.route('/dekripsi', methods=['POST', 'GET'])
+def dekripsi():
+    if request.method == 'POST':
+        input_text = request.form['plaintext']
+        input_key = request.form['key']
+        cipher = AESCipher(input_key)
+        decrypted = cipher.decrypt(input_text)
+        return render_template('dekripsi.html', submit=True, plaintext=decrypted)
+    return render_template('dekripsi.html', submit=False, variabel=1234)
+
+app.run(debug=True)
